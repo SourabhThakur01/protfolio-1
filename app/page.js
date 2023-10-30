8bb8e2459ad76397ea2bb6b9d3162d722717b4d3
SourabhThakur01 <119710836+SourabhThakur01@users.noreply.github.com>
2023-10-29 23:35:06 -0700

    Update page.js

--- app/page.js
+++ app/page.js
@@ -2,12 +2,14 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main>
-      <div className='text-left'>
-        <button>Home</button>
-        <button>Skills</button>
-        <button>Projects</button>
-        <button>Say Hi!</button>
+    <main className='bg-white text-black l-10'>
+      <div className='ml-[300px] mr-[300px]'>
+        <div className='text-right'>
+          <button className='text-bold mr-10 mt-3'>Home</button>
+          <button className='text-bold mr-10 mt-3'>Skills</button>
+          <button className='text-bold mr-10 mt-3'>Projects</button>
+          <button className='text-bold mr-5 mt-3 border rounded-full bg-gray-700 text-white p-1 pl-2 pr-2'>Say Hi!</button>
+        </div>
       </div>
     </main>
   )
