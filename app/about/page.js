c545a49e98b51a1fa51accefa3b9d2da6293d0cf
SourabhThakur01 <119710836+SourabhThakur01@users.noreply.github.com>
2023-11-27 00:50:46 -0800

    Update page.js

--- app/about/page.js
+++ app/about/page.js
@@ -4,9 +4,9 @@ export default function About() {
 
     return (
         <main>
-            <div className='flex justify-center items-center'>
+            <div className='flex justify-center items-center font-sans'>
                 <div className="border border-4 border-y-purple-600 max-w-fit rounded-md border-gray-900 bg-gray-900 p-4 mt-5 text-white shadow-xl shadow-black">
-                    <p className='text-center text-xl mt-5'>Hi! I am Sourabh Thakur</p>
+                    <p className='text-center text-2xl mt-5 font-serif'>Hi! I am Sourabh Thakur</p>
                     <div className='flex justify-center space-x-14 mb-8 px-14'>
                         <p className=' max-w-lg mt-10'>I am a dedicated lifelong learner, passionately engaged in various fields, with a rich tapestry of skills honed in last some years. I enjoyed my work and completely get into it untill its not done or i am not satisafied. My journey has been characterized by a relentless pursuit of knowledge and mastery in my areas of expertise. I am eager to leverage these skills in a professional setting, aiming to forge a successful career and a bright future with a happy and relaxed mindset. Known for my clear-mindedness and team-oriented approach, I am committed to further honing my abilities. By continuously enhancing my skills, I seek to bring clarity and excellence to my career path.</p>
                         <p className=' max-w-lg mt-10'> My focus is not just on personal growth but also on contributing significantly to any team I join, driving collective success through individual excellence and make chilled environmnet. Furthermore, my strong communication skills enable me to articulate complex ideas clearly, fostering understanding and collaboration among diverse teams. I also place a high value on ethics and integrity in my professional conduct. I believe that honesty, transparency, and accountability are key to building trust and lasting relationships in any workplace. As I continue to advance in my career, I am committed to making a positive impact not only through my technical skills but also through my character and leadership qualities.</p>
