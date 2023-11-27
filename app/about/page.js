bfbd564da7029a28de4d74f2faaa81d1af3e6240
SourabhThakur01 <119710836+SourabhThakur01@users.noreply.github.com>
2023-11-26 23:58:55 -0800

    Update page.js

--- app/about/page.js
+++ app/about/page.js
@@ -7,9 +7,8 @@ export default function About() {
             <div className='flex justify-center items-center'>
                 <div className="border border-4 border-y-purple-600 max-w-fit rounded-md border-gray-900 bg-gray-900 p-4 mt-5 text-white shadow-xl shadow-black">
                     <p className='text-center text-xl mt-5'>Hi! I am Sourabh Thakur</p>
-                    <div className='flex justify-center space-x-12 mb-8 px-10'>
-                        <p className='text-left max-w-lg mt-10'>jsdfbshj iab dadba dadahd ad ad adb adbadbkabdakbdia da diaidihadai dauhd iaudh aiud aud aiudhaidaiudg ad aiud aidgauid baiud aud aiud aidgaugd iadg aidgaiud aid aiudg aiudg aiugd aiugd iaudg aiudg aiudg aiudg iadgaidgaidgiadg aidg aidg iaudg iaud iaudg iadg aidg iadg iagdiadgiadgiadg a diagd iad iad aid iad ia d.</p>
-                        <p className='text-left max-w-lg mt-10'>jsdfbshj iab dadba dadahd ad ad adb adbadbkabdakbdia da diaidihadai dauhd iaudh aiud aud aiudhaidaiudg ad aiud aidgauid baiud aud aiud aidgaugd iadg aidgaiud aid aiudg aiudg aiugd aiugd iaudg aiudg aiudg aiudg iadgaidgaidgiadg aidg aidg iaudg iaud iaudg iadg aidg iadg iagdiadgiadgiadg a diagd iad iad aid iad ia d.</p>
+                    <div className='flex justify-center mb-8 px-20'>
+                        <p className='text-left max-w-4xl mt-10'>I am a dedicated lifelong learner, passionately engaged in various fields, with a rich tapestry of skills honed over many years. My journey has been characterized by a relentless pursuit of knowledge and mastery in my areas of expertise. I am eager to leverage these skills in a professional setting, aiming to forge a successful career and a bright future. Known for my clear-mindedness and team-oriented approach, I am committed to further honing my abilities. By continuously enhancing my skills, I seek to bring clarity and excellence to my career path. My focus is not just on personal growth but also on contributing significantly to any team I join, driving collective success through individual excellence.</p>
                     </div>         
                 </div>
             </div>
