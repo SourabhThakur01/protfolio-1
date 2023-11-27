b5668515d8e28ec6f7afa877be970e8d4666f9d6
SourabhThakur01 <119710836+SourabhThakur01@users.noreply.github.com>
2023-11-26 19:05:39 -0800

    Update page.js

--- app/profile/page.js
+++ app/profile/page.js
@@ -29,11 +29,8 @@ export default function Profile() {
              <a href='https://www.instagram.com/sourabh.thakur_01/'>
                <img src='./imgs/instagram.png' className='w-[40px] h-[40px] mr-7 transform transition-transform duration-300 hover:scale-125'/>
              </a>
-             <a href='https://www.instagram.com/sourabh.thakur_01/'>
-               <img src='./imgs/twitter.png' className='w-[40px] h-[40px] mr-7 transform transition-transform duration-300 hover:scale-125'/>
-             </a>
              <a href='https://www.linkedin.com/in/sourabh-thakur2530/'>
-               <img src='./imgs/linkdn.png' className='w-[40px] h-[40px] mr-7 transform transition-transform duration-300 hover:scale-125'/>
+               <img src='./imgs/Linkdn.png' className='w-[40px] h-[40px] mr-7 transform transition-transform duration-300 hover:scale-125'/>
              </a>
            </div>
         );
