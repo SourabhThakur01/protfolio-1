e20623fbdbf2ab71690921aaaeb2e30511ed0f9a
SourabhThakur01 <119710836+SourabhThakur01@users.noreply.github.com>
2023-12-11 02:27:21 -0800

    Update page.js

--- app/header/page.js
+++ app/header/page.js
@@ -36,10 +36,10 @@ const Header = () => {
                 <button onClick={() => scrollToSection(contactRef)} className='text-bold mr-14 mt-3 border border-2 border-purple-600 rounded-full bg-gray-900 text-white p-1 pl-2 pr-2'>Say Hi!</button>
                 <p className='border-b-2 border-black p-1'></p>
                 </div>
-                <div className="md:hidden flex items-left p-3">
+                <div className="md:hidden absolute ">
                     <details className="dropdown">
-                        <summary className="text-bold btn bg-white">☰</summary>
-                        <ul className="p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-52">
+                        <summary className="text-bold text-3xl border-white btn bg-white">☰</summary>
+                        <ul className="p-2 text-md shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-52 text-white">
                             <li><button onClick={() => scrollToSection(skillsRef)}>Skills</button></li>
                             <li><button onClick={() => scrollToSection(qualificationsRef)}>Qualifications</button></li>
                             <li><button onClick={() => scrollToSection(projectsRef)}>Projects</button></li>
