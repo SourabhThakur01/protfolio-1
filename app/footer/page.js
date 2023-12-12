da1f8b649c32dc326a3c8717fd09613baaa06f5d
SourabhThakur01 <119710836+SourabhThakur01@users.noreply.github.com>
2023-12-12 02:43:59 -0800

    Update page.js

--- app/footer/page.js
+++ app/footer/page.js
@@ -23,13 +23,13 @@ const Footer = forwardRef((props, ref) => {
         <footer ref={ref} className="bg-gradient-to-t from-gray-900 to-gray-700 p-14 pb-20">
             <p className='text-2xl font-semibold font-serif text-center text-white'>SAY HELLO!</p>
             <form>
-                <div className='md:flex md:justify-center grid grid-cols-1 justify-items-center mt-5'>
-                <div className='md:mt-20'>
+                <div className='md:flex md:justify-center grid grid-cols-1 justify-items-center mt-10'>
+                <div className='md:mt-20 '>
                     <input
                         type="text"
                         name="firstName"
                         placeholder="First Name"
-                        className='border border-white rounded-md bg-gray-900 text-white p-1 pl-2 pr-2 ml-10'
+                        className='border border-white rounded-md bg-gray-900 text-white p-1 pl-2 pr-2 md:ml-10'
                         value={formData.firstName}
                         onChange={handleChange}
                     /><br/>
@@ -37,7 +37,7 @@ const Footer = forwardRef((props, ref) => {
                         type="text"
                         name="lastName"
                         placeholder="Last Name"
-                        className='border border-white rounded-md bg-gray-900 text-white p-1 pl-2 pr-2 mt-10 ml-10'
+                        className='border border-white rounded-md bg-gray-900 text-white p-1 pl-2 pr-2 mt-10 md:ml-10'
                         value={formData.lastName}
                         onChange={handleChange}
                     /><br/>
@@ -45,7 +45,7 @@ const Footer = forwardRef((props, ref) => {
                 <textarea
                     name="message"
                     placeholder="Your Message"
-                    className='border border-white rounded-md bg-gray-900 text-white mt-10 p-1 pl-2 pr-2 ml-10 md:ml-[120px] h-[200px] w-[250px]'
+                    className='border border-white rounded-md bg-gray-900 text-white mt-10 p-1 pl-2 pr-2  md:ml-[120px] h-[200px] w-[250px]'
                     value={formData.message}
                     onChange={handleChange}
                 ></textarea>
