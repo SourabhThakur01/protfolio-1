9cfecef611ca9a9e59885966864bbff5bedf9d55
SourabhThakur01 <119710836+SourabhThakur01@users.noreply.github.com>
2023-12-12 00:46:01 -0800

    Update qualifications.js

--- app/qualifications.js
+++ app/qualifications.js
@@ -2,7 +2,7 @@ import React, { forwardRef } from 'react';
 
 const Qualifications = forwardRef((props, ref) => (
     <a href='./qualifications' ref={ref} className=''>
-        <div className='border border-black shadow-black shadow-lg bg-gray-900 p-10 max-w-md md:p-20 md:w-[400px] text-white font-bold text-4xl border border-y-4 border-y-purple-600 rounded-lg hover hover:shadow-lg hover:shadow-purple-600 raise'>Qualifications</div>
+        <div className='border border-black shadow-black shadow-lg bg-gray-900 p-10 max-w-md md:p-20 md:w-[400px] text-white font-bold font-serif text:2xl md:text-4xl border border-y-4 border-y-purple-600 rounded-lg hover hover:shadow-lg hover:shadow-purple-600 raise'>Qualifications</div>
     </a>
 ));
 
